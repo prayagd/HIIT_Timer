3003da3e53e9446ad19148a8b37760078b79cb47
prayag <prayagd21@gmail.com>
2020-04-05 17:52:52 +0530

    initial setup for the timer

--- src/App.js
+++ src/App.js
@@ -1,26 +1,98 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			initialDuration: 20,
+			initialBreak: 10
+		};
+		this.handleDuration = this.handleDuration.bind(this);
+		this.handleBreak = this.handleBreak.bind(this);
+	}
+
+	handleDuration(e) {
+		let { initialDuration } = this.state;
+		if (e.target.value === '+') {
+			this.setState({
+				initialDuration: initialDuration + 1
+			});
+		}
+		if (e.target.value === '-') {
+			this.setState({
+				initialDuration: initialDuration - 1
+			});
+		}
+	}
+
+	handleBreak(e) {
+		let { initialBreak } = this.state;
+		if (e.target.value === '+') {
+			this.setState({
+				initialBreak: initialBreak + 1
+			});
+		}
+		if (e.target.value === '-') {
+			this.setState({
+				initialBreak: initialBreak - 1
+			});
+		}
+	}
+
+	handleSet(e) {
+		let { initialBreak } = this.state;
+		if (e.target.value === '+') {
+			this.setState({
+				initialBreak: initialBreak + 1
+			});
+		}
+		if (e.target.value === '-') {
+			this.setState({
+				initialBreak: initialBreak - 1
+			});
+		}
+	}
+
+	handleDisplay(e) {}
+
+	render() {
+		return (
+			<div>
+				<div>
+					<p>Sets</p>
+					<button>-</button>
+					<span>16</span>
+					<button>+</button>
+				</div>
+				<div>
+					<p>Workout Duration</p>
+					<button onClick={this.handleDuration} value="-">
+						-
+					</button>
+					<span>00:{this.state.initialDuration}</span>
+					<button onClick={this.handleDuration} value="+">
+						+
+					</button>
+				</div>
+				<div>
+					<p>Rest</p>
+					<button onClick={this.handleBreak} value="-">
+						-
+					</button>
+					<span>00:{this.state.initialBreak}</span>
+					<button onClick={this.handleBreak} value="+">
+						+
+					</button>
+				</div>
+				<button>Start</button>
+				<button>Reset</button>
+				<div id="display">
+					<h2>Work</h2>
+					<span>00:00</span>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
