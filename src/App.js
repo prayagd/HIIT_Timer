c266565c5b8fcdfe319ebe5a79be8afb29e9552d
prayag <prayagd21@gmail.com>
2020-04-06 22:16:52 +0530

    created event handlers

--- src/App.js
+++ src/App.js
@@ -5,64 +5,88 @@ class App extends React.Component {
 		super(props);
 		this.state = {
 			initialDuration: 20,
-			initialBreak: 10
+			initialBreak: 10,
+			initialSet: 1
 		};
 		this.handleDuration = this.handleDuration.bind(this);
 		this.handleBreak = this.handleBreak.bind(this);
+		this.handleSet = this.handleSet.bind(this);
+		this.handleStart = this.handleStart.bind(this);
+		this.handleInit = this.handleInit.bind(this);
 	}
 
 	handleDuration(e) {
 		let { initialDuration } = this.state;
-		if (e.target.value === '+') {
-			this.setState({
-				initialDuration: initialDuration + 1
-			});
-		}
-		if (e.target.value === '-') {
-			this.setState({
-				initialDuration: initialDuration - 1
-			});
+		if (initialDuration > 0) {
+			if (e.target.value === '+') {
+				this.setState({
+					initialDuration: initialDuration + 1
+				});
+			}
+			if (e.target.value === '-') {
+				this.setState({
+					initialDuration: initialDuration - 1
+				});
+			}
 		}
 	}
 
 	handleBreak(e) {
 		let { initialBreak } = this.state;
-		if (e.target.value === '+') {
-			this.setState({
-				initialBreak: initialBreak + 1
-			});
-		}
-		if (e.target.value === '-') {
-			this.setState({
-				initialBreak: initialBreak - 1
-			});
+		if (initialBreak > 0) {
+			if (e.target.value === '+') {
+				this.setState({
+					initialBreak: initialBreak + 1
+				});
+			}
+			if (e.target.value === '-') {
+				this.setState({
+					initialBreak: initialBreak - 1
+				});
+			}
 		}
 	}
 
 	handleSet(e) {
-		let { initialBreak } = this.state;
-		if (e.target.value === '+') {
-			this.setState({
-				initialBreak: initialBreak + 1
-			});
-		}
-		if (e.target.value === '-') {
-			this.setState({
-				initialBreak: initialBreak - 1
-			});
+		let { initialSet } = this.state;
+		if (initialSet > 0) {
+			if (e.target.value === '+') {
+				this.setState({
+					initialSet: initialSet + 1
+				});
+			}
+			if (e.target.value === '-') {
+				this.setState({
+					initialSet: initialSet - 1
+				});
+			}
 		}
 	}
 
 	handleDisplay(e) {}
+
+	handleStart(e) {}
+
+	handleInit(e) {
+		this.setState({
+			initialDuration: 20,
+			initialBreak: 10,
+			initialSet: 1
+		});
+	}
 
 	render() {
 		return (
 			<div>
 				<div>
 					<p>Sets</p>
-					<button>-</button>
-					<span>16</span>
-					<button>+</button>
+					<button onClick={this.handleSet} value="-">
+						-
+					</button>
+					<span>{this.state.initialSet}</span>
+					<button onClick={this.handleSet} value="+">
+						+
+					</button>
 				</div>
 				<div>
 					<p>Workout Duration</p>
@@ -84,8 +108,8 @@ class App extends React.Component {
 						+
 					</button>
 				</div>
-				<button>Start</button>
-				<button>Reset</button>
+				<button onClick={this.handleStart}>Start</button>
+				<button onClick={this.handleInit}>Reset</button>
 				<div id="display">
 					<h2>Work</h2>
 					<span>00:00</span>
